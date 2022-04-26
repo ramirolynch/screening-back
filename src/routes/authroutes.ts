3c1ca06f8f4766ca14f435c376fda91656b5d110
ramirolynch <ramirolynch@yahoo.com>
2022-04-25 19:39:58 -0500

    modified tables.sql, added table no_match

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -48,6 +48,41 @@ routes.post("/signup", (req, res) => {
   });
 });
 
+routes.post("/matchreview", (req, res) => {
+  const matchrev = {
+    list_id: req.body.list_id,
+    searched_name: req.body.searched_name,
+    matched_name: req.body.matched_name,
+    score: req.body.score,
+    positive_match: req.body.positive_match,
+    review_comments: req.body.review_comments,
+    user_id: req.body.user_id,
+  };
+
+  db.oneOrNone("select * from match_reviews where list_id = ${list_id}", {
+    list_id: req.body.list_id,
+  }).then((match) => {
+    if (match) {
+      return res
+        .status(400)
+        .send("There is a match review with that list_id already.");
+    }
+
+    db.one(
+      "INSERT INTO match_reviews(list_id, searched_name, matched_name, score, positive_match, review_comments, user_id) VALUES(${list_id}, ${searched_name}, ${matched_name}, ${score}, ${positive_match}, ${review_comments}, ${user_id}) returning id",
+      matchrev
+    )
+      .then((id) => {
+        return db.oneOrNone("SELECT * FROM match_reviews WHERE id = ${id}", {
+          id: id.id,
+        });
+      })
+      .then((data) => res.json(data))
+
+      .catch((error) => res.status(500).send(error));
+  });
+});
+
 routes.post("/login", (req, res) => {
   db.oneOrNone("select id, email, password from users where email = ${email}", {
     email: req.body.email,
--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-drop table users;
+drop table users, match_reviews, no_match;
 
 create table users (
 	id serial primary key,
@@ -13,3 +13,26 @@ insert into users (first_name, last_name, email, password) values ('Matt', 'Fant
 insert into users (first_name, last_name, email, password) values ('Henry', 'Overholt', 'henry@gmail.com', 'henrypass');
 insert into users (first_name, last_name, email, password) values ('Dana', 'Killeen', 'dana@gmail.com', 'danapass');
 insert into users (first_name, last_name, email, password) values ('Sudha', 'Vallabhapurapu', 'sudha@gmail.com', 'sudhapass');
+
+create table match_reviews (
+	id serial primary key,
+	list_id varchar(200),
+	searched_name varchar(300) not null,
+	matched_name varchar(300) not null,
+	score numeric not null,
+	positive_match boolean not null,
+	review_comments text,
+	user_id integer references users
+	
+);
+
+insert into match_reviews (list_id,searched_name,matched_name,score,positive_match, review_comments, user_id) values('test_list_id','Rolando','Rolando Perez',93,false, 'only matched the first name, so its a negative match',1);
+
+create table no_match (
+	id serial primary key,
+	searched_name text not null,
+	screening_ts timestamp not null default now(),
+	user_id integer references users
+);
+
+insert into no_match (searched_name,user_id) values ('Pedro Gonzalez',1);
