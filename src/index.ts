62cf28c9adc9616a3e32906b2ae9865c3f57e90b
ramirolynch <ramirolynch@yahoo.com>
2022-05-07 13:32:03 -0500

    added helmet

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import express from "express"; // require the cors module
 import cors from "cors";
 import authroutes from "./routes/authroutes";
 import matchroutes from "./routes/matchroutes";
-
+import helmet from "helmet";
 // creates an instance of an Express server
 const app = express();
 // enable Cross Origin Resource Sharing so this API can be used from web-apps on other domains
 app.use(cors());
+app.use(helmet());
 // allow POST and PUT requests to use JSON bodies
 app.use(express.json()); // define the port
 app.use("/", authroutes);
